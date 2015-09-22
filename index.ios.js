246f39b4d482bf26b63c6b90e7aa71eef226d577
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-09-22 07:00:00 -0400

    get some data into app

--- index.ios.js
+++ index.ios.js
@@ -1,50 +1,69 @@
 /* @flow */
 'use strict';
 
+var plants = require('./data/plants.json');
 var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
+  ListView
 } = React;
 
 var PetSafePlants = React.createClass({
-  render: function() {
+
+  getInitialState: function() {
+    var ds = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2
+    });
+    return {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(plants),
+      loaded: true,
+    });
+  },
+
+  renderLoadingView: function () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+          Loading plants...
         </Text>
       </View>
     );
+  },
+
+  renderPlantRow: function(plant) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{plant.name}</Text>
+      </View>
+    );
+  },
+
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        initialListSize={40}
+        renderRow={this.renderPlantRow}
+        style={styles.container}
+      />
+    );
   }
+
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+var styles = StyleSheet.create({});
 
 AppRegistry.registerComponent('PetSafePlants', () => PetSafePlants);
